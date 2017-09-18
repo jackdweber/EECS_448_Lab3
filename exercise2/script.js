6449822c66437cc45e3de231977bcaa16b2019cc
Jack Weber <jackweber@ku.edu>
2017-09-18 13:29:07 -0500

    Started work on exercise 2: Added html elements, and set up array of images.

---
+++ exercise2/script.js
@@ -0,0 +1,20 @@
+var img = document.getElementById('img1')
+
+var imgs = ["https://upload.wikimedia.org/wikipedia/commons/5/55/Atelopus_zeteki1.jpg", 
+            "http://kids.nationalgeographic.com/content/dam/kids/photos/articles/Other%20Explore%20Photos/R-Z/Wacky%20Weekend/Frogs/ww-frogs-budgetts.adapt.945.1.jpg",
+            "http://kids.nationalgeographic.com/content/dam/kids/photos/articles/Other%20Explore%20Photos/R-Z/Wacky%20Weekend/Frogs/ww-frogs-waxy-monkey-tree.adapt.945.1.jpg",
+            "http://www.artfido.com/blog/wp-content/uploads/2016/11/016.jpg",
+            "http://voxpopulinews.net/wp-content/uploads/2016/03/Mink-frog.jpg"
+            ]
+
+var next_image = function() {
+
+}
+
+var prev_image = function(){
+
+}
+
+var new_image = function(imgsrc){
+
+}
