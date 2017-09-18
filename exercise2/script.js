040a355864f8b185c3d62462f718a225046f7aac
Jack Weber <jackweber@ku.edu>
2017-09-18 13:36:43 -0500

    1) Finished js functions 2) finished exercise

--- exercise2/script.js
+++ exercise2/script.js
@@ -1,5 +1,3 @@
-var img = document.getElementById('img1')
-
 var imgs = ["https://upload.wikimedia.org/wikipedia/commons/5/55/Atelopus_zeteki1.jpg", 
             "http://kids.nationalgeographic.com/content/dam/kids/photos/articles/Other%20Explore%20Photos/R-Z/Wacky%20Weekend/Frogs/ww-frogs-budgetts.adapt.945.1.jpg",
             "http://kids.nationalgeographic.com/content/dam/kids/photos/articles/Other%20Explore%20Photos/R-Z/Wacky%20Weekend/Frogs/ww-frogs-waxy-monkey-tree.adapt.945.1.jpg",
@@ -7,14 +5,29 @@ var imgs = ["https://upload.wikimedia.org/wikipedia/commons/5/55/Atelopus_zeteki
             "http://voxpopulinews.net/wp-content/uploads/2016/03/Mink-frog.jpg"
             ]
 
-var next_image = function() {
+var COUNT = 0
+var COUNT_TOTAL = imgs.length
 
+var next_image = function() {
+    var new_count = COUNT + 1
+    if(new_count >= COUNT_TOTAL){
+        new_count = 0
+    }
+    new_image(imgs[new_count])
+    COUNT = new_count
 }
 
 var prev_image = function(){
-
+    var new_count = COUNT - 1
+    if(new_count < 0){
+        new_count = COUNT_TOTAL - 1
+    }
+    new_image(imgs[new_count])
+    COUNT = new_count
 }
 
 var new_image = function(imgsrc){
-
+    document.getElementById('img1').src = imgsrc
 }
+
+new_image(imgs[COUNT])
