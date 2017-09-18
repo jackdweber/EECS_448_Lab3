772a8fd9c7ecef26506d9c06792bd628d1a9ecf1
Jack Weber <jackweber@ku.edu>
2017-09-18 13:15:46 -0500

    1) Finished validate_password function, finishing exercise 1

--- exercise1/script.js
+++ exercise1/script.js
@@ -1,4 +1,17 @@
 // Function to validate password.
 var validate_password = function(){
-    alert('Connected!')
+    var pass1 = document.getElementById('pass1').value
+    var pass2 = document.getElementById('pass2').value
+
+    if(pass1.length < 8){
+        alert('Passsword must be 8 characters long.')
+        return
+    }
+
+    if(pass1 !== pass2){
+        alert('Passwords must be the same.')
+        return
+    }
+
+    alert('Congrats the passwords are equal!')
 }
